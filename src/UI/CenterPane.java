a86aff2b6034e011c64430e15b59e686661827ff
skirkwood1 <simonjameskirkwood@gmail.com>
2020-01-01 22:37:46 -0500

    Added mouse-drag panning to images
    Removed scroll bars as well because they are now unnecessary.

--- src/UI/CenterPane.java
+++ src/UI/CenterPane.java
@@ -6,6 +6,8 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -83,6 +85,46 @@ public class CenterPane extends JPanel {
                 setCardText(card.getText());
             }
         });
+
+        componentImage.setAutoscrolls(true);
+
+        MouseAdapter ma = new MouseAdapter() {
+
+            private Point origin;
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                origin = new Point(e.getPoint());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (origin != null) {
+                    JViewport viewPort = (JViewport) SwingUtilities.getAncestorOfClass(JViewport.class, componentImage);
+                    if (viewPort != null) {
+                        int deltaX = origin.x - e.getX();
+                        int deltaY = origin.y - e.getY();
+
+                        Rectangle view = viewPort.getViewRect();
+                        view.x += deltaX;
+                        view.y += deltaY;
+
+                        componentImage.scrollRectToVisible(view);
+                    }
+                }
+            }
+
+        };
+
+        componentImage.addMouseListener(ma);
+        componentImage.addMouseMotionListener(ma);
+
+        imagePane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        imagePane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 
     }
 
