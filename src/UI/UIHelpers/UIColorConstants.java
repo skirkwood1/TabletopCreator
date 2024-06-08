957a839f9fde9a94e5d257ad0d62d3207458d470
Simon James Kirkwood <48489642+skirkwood1@users.noreply.github.com>
2024-06-08 01:21:55 -0400

    Created skeletons for player management and board pane overlay buttons

---
+++ src/UI/UIHelpers/UIColorConstants.java
@@ -0,0 +1,17 @@
+package UI;
+
+import java.awt.*;
+
+public class UIColorConstants {
+    public static Color buttonUnselected = new Color(240, 240, 240);
+    public static Color buttonHovered = new Color(225,225,225);
+    public static Color buttonSelected = new Color(200, 200, 200);
+
+    public static Color background = new Color(240,240,240);
+
+    public static Color outlines = new Color(0,0,0);
+
+    public static void setDarkMode(boolean darkMode) {
+        background = new Color(20, 20, 20);
+    }
+}
